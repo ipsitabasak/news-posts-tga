be3186ebd7ac058bf0bdcbed848856a822f78cf1
Ipsita Basak <ipsita.29.10@gmail.com>
2022-05-16 15:56:18 +0530

    Update util.test.js
    Reverted the file back to normal

--- src/__tests__/util.test.js
+++ src/__tests__/util.test.js
@@ -4,6 +4,5 @@ describe('getUpVoteCount function', () => {
   it('works correctly', () => {
     const count = getUpVoteCount('334334');
     expect(count).toEqual(0);
-    expect(count).toEqual(0);
   });
 });
