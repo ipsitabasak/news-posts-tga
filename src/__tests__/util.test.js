a4b38ecc59dbb3efcaf364eee1e73934031549b7
Ipsita Basak <ipsita.29.10@gmail.com>
2022-05-16 15:48:26 +0530

    Update util.test.js

--- src/__tests__/util.test.js
+++ src/__tests__/util.test.js
@@ -4,5 +4,6 @@ describe('getUpVoteCount function', () => {
   it('works correctly', () => {
     const count = getUpVoteCount('334334');
     expect(count).toEqual(0);
+    expect(count).toEqual(0);
   });
 });
